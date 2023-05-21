f1adee1a02abd5d92384257d8a26d610548774fa
Kyungmi Kim <rudal9983@gmail.com>
2023-05-21 21:27:46 +0200

    document optimizations

--- generator.py
+++ generator.py
@@ -7,12 +7,14 @@ matrix_size = number_of_agents + number_of_test_nodes
 
 graph_matrix = np.zeros((matrix_size, matrix_size), dtype=np.int8)
 
+# Optimzation: Do not store whole matrix, we're only interested in the relation of agents to test nodes (top right part of matrix)
 for x in range(0, number_of_agents):
     for y in range(number_of_agents, number_of_agents + number_of_test_nodes):
         rand = random.randint(0,1)
         graph_matrix[x][y] = rand
         graph_matrix[y][x] = rand
 
+# Optimization: Store as binary for smaller size
 np.savetxt('./results/1', graph_matrix, fmt='%i')
 
 print(graph_matrix)
