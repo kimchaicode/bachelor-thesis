6dc2c480b2fd56b81c5cf3999fbe4af7f92b5f09
Kyungmi Kim <rudal9983@gmail.com>
2023-05-21 21:16:15 +0200

    generate matrix of agents+tests size

---
+++ generator.py
@@ -0,0 +1,17 @@
+import random
+import numpy as np
+
+number_of_agents = 7
+number_of_test_nodes = 5
+matrix_size = number_of_agents + number_of_test_nodes
+
+graph_matrix = np.zeros((matrix_size, matrix_size), dtype=np.int8)
+
+for x in range(0, number_of_agents):
+    for y in range(number_of_agents, number_of_agents + number_of_test_nodes):
+        rand = random.randint(0,1)
+        graph_matrix[x][y] = rand
+        graph_matrix[y][x] = rand
+
+print(graph_matrix)
+
