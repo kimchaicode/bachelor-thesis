5ad7ba4921b29c3969980a65f3b90c75d55ea41f
Kyungmi Kim <rudal9983@gmail.com>
2023-08-05 15:20:41 +0200

    generator: write data sets in one row

--- generator/generator.py
+++ generator/generator.py
@@ -1,19 +1,21 @@
 import numpy as np
 
-number_of_agents = 7
-number_of_test_nodes = 5
+number_of_agents = 3
+number_of_test_nodes = 2
 
 graph_matrix = np.random.randint(2, size=(number_of_test_nodes, number_of_agents))
-
-# Optimization: Store in one file
-# Optimization: Store as binary for smaller size
-np.savetxt('./results/1', graph_matrix, fmt='%i')
-print(graph_matrix)
-
 infection_vector = np.random.randint(2, size=(number_of_agents, 1))
-np.savetxt('./results/1.infection', infection_vector, fmt='%i')
-print(infection_vector)
+infection_vector_as_string = "".join(map(str, infection_vector.flatten()))
+infection_vector_as_binary_number = int(infection_vector_as_string, 2)
 
 test_result_vector = np.dot(graph_matrix, infection_vector)
-np.savetxt('./results/1.test_result', test_result_vector, fmt='%i')
-print(test_result_vector)
+
+# Write the data in the following way into the same file linearly, i.e. 1 row per data point
+# graph_matrix, test_result_vector, infection_vector as class number (binary)
+# Example: 0,1,1,1,0,1, 1,2,               5
+#         |graph_matrix|test_result_vector|class_number (infection vector as binary number)|
+with open("./results/agents.data", "w") as file:
+    np.savetxt(file, graph_matrix, fmt='%i', delimiter=",", newline=",")
+    np.savetxt(file, test_result_vector, fmt='%i', delimiter=",", newline=",")
+    np.savetxt(file, [infection_vector_as_binary_number], fmt='%i')
+
