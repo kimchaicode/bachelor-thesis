696f157415da70d92422a66ab9a0b7ae5006cc32
Kyungmi Kim <rudal9983@gmail.com>
2023-08-13 19:22:07 +0200

    generator: generate datasets for binary classfication
    each sample is made up of (test assignment graph, test result vector, agent identifier)

--- generator/generator.py
+++ generator/generator.py
@@ -1,7 +1,7 @@
 # Write the data in the following way into the same file linearly, i.e. 1 row per data point
 # graph_matrix, test_result_vector, infection_vector as class number (binary)
-# Example: 0,1,1,1,0,1, 1,2,               5
-#         |graph_matrix|test_result_vector|class_number (infection vector as binary number)|
+# Example: 0,1,1,1,0,1, 1,2,               1
+#         |graph_matrix|test_result_vector|agent identifier
 
 # TODO: Questions
 # 1. How many agents do we want? like 1000, increase agents number, try 100 at first
@@ -17,11 +17,15 @@ import numpy as np
 # Allow for variable configuration of agents and test nodes with these parameters
 min_agents, max_agents = 10, 10
 min_test_nodes, max_test_nodes = 5, 5
+# Later: 1-5% infected
+min_infected, max_infected = 1, 1
 
-number_of_samples = 150
+# we generate a row per agent, because we do binary classification on the infection status of each agent
+# in total there will be number_of_samples_per_agent * max_agents rows
+number_of_samples_per_agent = 150
 
 with open("./results/agents.data", "w") as file:
-    for i in range(number_of_samples):
+    for i in range(number_of_samples_per_agent):
         # Random configuration for number of agents
         number_of_agents = random.randint(min_agents, max_agents)
         # Random configuration for test nodes, but do not allow more test nodes than agents
@@ -32,18 +36,23 @@ with open("./results/agents.data", "w") as file:
         graph_matrix = np.random.randint(2, size=(number_of_test_nodes, number_of_agents))
         graph_matrix = np.pad(graph_matrix, [(0, max_test_nodes - number_of_test_nodes), (0, max_agents - number_of_agents)], mode='constant', constant_values=0)
 
-        # Pad the infection_vector, so that we can calculate the dot product with the padded graph_matrix
-        infection_vector = np.random.randint(2, size=(number_of_agents, 1))
-        infection_vector = np.pad(infection_vector, [(0, max_agents - number_of_agents), (0, 0)], mode='constant', constant_values=0)
+        number_of_infected = random.randint(min_infected, max_infected)
+        # Create an array with a specific number of 1s (infected), and rest 0s
+        # See https://stackoverflow.com/questions/19597473/binary-random-array-with-a-specific-proportion-of-ones
+        infection_vector = np.array([1] * number_of_infected + [0] * (number_of_agents - number_of_infected)).reshape(number_of_agents, 1)
+        np.random.shuffle(infection_vector)
 
-        # Reversing the string is not necessary, but it will make vectors with less agents have a smaller "class number"
-        # which is more intuitive.
-        infection_vector_as_string = "".join(map(str, infection_vector.flatten()))
-        infection_vector_as_binary_number = int(infection_vector_as_string[::-1], 2)
+        # Pad the infection_vector, so that we can calculate the dot product with the padded graph_matrix
+        infection_vector = np.pad(infection_vector, [(0, max_agents - number_of_agents), (0, 0)], mode='constant', constant_values=0)
         test_result_vector = np.dot(graph_matrix, infection_vector)
 
-        # Output each sample in one row, comma-separated
-        np.savetxt(file, graph_matrix, fmt='%i', delimiter=",", newline=",")
-        np.savetxt(file, test_result_vector, fmt='%i', delimiter=",", newline=",")
-        np.savetxt(file, [infection_vector_as_binary_number], fmt='%i')
+        # For each agent, write one sample that has the agent as the identifier, and the infection status as the output class
+        for agent_identifier in range(number_of_agents):
+            infection_status = infection_vector[agent_identifier]
+
+            # Output each sample in one row, comma-separated
+            np.savetxt(file, graph_matrix, fmt='%i', delimiter=",", newline=",")
+            np.savetxt(file, test_result_vector, fmt='%i', delimiter=",", newline=",")
+            np.savetxt(file, [agent_identifier], fmt='%i', delimiter=",", newline=",")
+            np.savetxt(file, infection_status, fmt='%i')
 
