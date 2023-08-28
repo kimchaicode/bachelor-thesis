d6127851c2376e1f2750df8c486c82183fc9ffee
Kyungmi Kim <rudal9983@gmail.com>
2023-08-28 22:35:58 +0200

    sklearn: logistic regression basic example

---
+++ neural_network/scikitlearn.py
@@ -0,0 +1,30 @@
+# TODO After we have a working solution, we can compare all classifiers from
+# https://scikit-learn.org/stable/auto_examples/classification/plot_classifier_comparison.html
+
+import numpy as np
+
+from sklearn import metrics
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+
+
+# From generator.py
+max_agents = 10
+max_test_nodes = 5
+
+num_input_nodes = max_agents * max_test_nodes + max_test_nodes + 1
+
+data = np.genfromtxt("./data/agents.data", delimiter=',', dtype=int)
+
+# TODO: Optimize splitting
+X = np.array([row[:len(row) - 1] for row in data])
+y = np.array([[row[len(row) - 1]] for row in data])
+
+X_train, X_validation, y_train, y_validation = train_test_split(X, y, test_size=0.2)
+
+# LogisticRegression
+reg_log = LogisticRegression()
+reg_log.fit(X_train, y_train)
+y_pred = reg_log.predict(X_validation)
+
+print(metrics.classification_report(y_validation, y_pred))
