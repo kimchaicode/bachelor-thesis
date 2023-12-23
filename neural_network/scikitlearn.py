e25288268a9ab6bb8ded6d879664d82267929988
Kyungmi Kim <rudal9983@gmail.com>
2023-12-23 19:20:25 +0100

    test different batch sizes

--- neural_network/scikitlearn.py
+++ neural_network/scikitlearn.py
@@ -27,17 +27,17 @@ y_original = np.array([row[len(row) - 1] for row in data])
 # sample_sizes = [5000, 10000, 15000, 20000, 25000, 30000, 35000, 40000, 45000, 50000]
 sample_size = 35000
 # hidden_layer_size = Config.num_input_nodes * 2
-hidden_layers = (Config.num_input_nodes,Config.num_input_nodes,)
-learning_rates = [
-    0.0005,
-    0.00075,
-    0.001, # default
-    0.00125,
-    0.0015
+# hidden_layers = (Config.num_input_nodes,Config.num_input_nodes,)
+batch_sizes = [
+    100,
+    200, # default
+    500,
+    1000,
+    5000
 ]
 
-for (index, learning_rate) in enumerate(learning_rates):
-    print(f"Training {learning_rate}...")
+for (index, batch_size) in enumerate(batch_sizes):
+    print(f"Training {batch_size}...")
 
     number_of_samples = sample_size * Config.max_agents
     X = X_original[:number_of_samples]
@@ -46,11 +46,11 @@ for (index, learning_rate) in enumerate(learning_rates):
     X_train, X_validation, y_train, y_validation = train_test_split(X, y, test_size=0.2)
     X_train, y_train = resample(X_train, y_train)
 
-    model = train_model(X_train, y_train, learning_rate=learning_rate)
+    model = train_model(X_train, y_train, batch_size=batch_size)
 
     y_pred = model.predict(X_validation)
     print(classification_report(y_validation, y_pred, zero_division=0.0))
 
-    RocCurveDisplay.from_estimator(model, X_validation, y_validation, name=str(learning_rate), ax=ax)
+    RocCurveDisplay.from_estimator(model, X_validation, y_validation, name=str(batch_size), ax=ax)
 
 plt.show()
