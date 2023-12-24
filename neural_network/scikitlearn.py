c79fd69c4642d766a1934651fc04a9b0b2fe6467
Kyungmi Kim <rudal9983@gmail.com>
2023-12-24 14:43:23 +0100

    add confusion matrix

--- neural_network/scikitlearn.py
+++ neural_network/scikitlearn.py
@@ -8,7 +8,7 @@ from config import Config
 import numpy as np
 import matplotlib.pyplot as plt
 
-from sklearn.metrics import classification_report, RocCurveDisplay
+from sklearn.metrics import classification_report, ConfusionMatrixDisplay, RocCurveDisplay
 from sklearn.model_selection import train_test_split
 
 from resampling import resample
@@ -28,29 +28,20 @@ y_original = np.array([row[len(row) - 1] for row in data])
 sample_size = 35000
 # hidden_layer_size = Config.num_input_nodes * 2
 # hidden_layers = (Config.num_input_nodes,Config.num_input_nodes,)
-batch_sizes = [
-    100,
-    200, # default
-    500,
-    1000,
-    5000
-]
 
-for (index, batch_size) in enumerate(batch_sizes):
-    print(f"Training {batch_size}...")
+number_of_samples = sample_size * Config.max_agents
+X = X_original[:number_of_samples]
+y = y_original[:number_of_samples]
 
-    number_of_samples = sample_size * Config.max_agents
-    X = X_original[:number_of_samples]
-    y = y_original[:number_of_samples]
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+X_train, y_train = resample(X_train, y_train)
 
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
-    X_train, y_train = resample(X_train, y_train)
+model = train_model(X_train, y_train)
 
-    model = train_model(X_train, y_train, batch_size=batch_size)
+y_pred = model.predict(X_test)
+print(classification_report(y_test, y_pred, zero_division=0.0))
 
-    y_pred = model.predict(X_test)
-    print(classification_report(y_test, y_pred, zero_division=0.0))
-
-    RocCurveDisplay.from_estimator(model, X_test, y_test, name=str(batch_size), ax=ax)
+RocCurveDisplay.from_estimator(model, X_test, y_test, ax=ax)
+confusion = ConfusionMatrixDisplay.from_predictions(y_test, y_pred)
 
 plt.show()
